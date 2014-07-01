5218ba4bb6f2c8f15375df5a7e3efa6435c634d7
John Davison <johncdavison@gmail.com>
2014-06-30 21:29:57 -0700

    Sinatra is 'cough' tweeting...

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+
+gem "sinatra"
+gem "datamapper"
+group :development do
+  gem "sqlite3"
+  gem "dm-sqlite-adapter"
+  gem "rerun"
+end
---
+++ app.rb
@@ -0,0 +1,63 @@
+require 'sinatra'
+require 'data_mapper'
+DataMapper.setup(:default, "sqlite:tweets.db")
+
+class Tweet
+  include DataMapper::Resource
+  property :id,          Serial
+  property :content, String
+  property :owner, String
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
+get "/" do
+  @tweets = tweet.all
+  erb :tweets
+end
+
+get "/tweets/new" do
+  @tweet = Tweet.new
+  erb :knew
+end
+
+get "/tweets/:id" do
+  @tweet = Tweet.get(params[id])
+  erb :tweet
+end
+
+post "/tweets" do
+  new_tweet = params[:tweet]
+  @tweet = Tweet.create(new_tweet_info)
+  if @tweet.saved
+    redirect "/"
+  else
+    erb :new
+  end
+end
+
+get "/tweets/edit/id" do
+  @tweet = Tweet.get(params[:id])
+  if @tweet
+    erb :edit
+  else
+    redirect "/404.html"
+  end
+end
+
+puts "/tweets/:id" do
+  @tweet = Tweet.get(params[:id])
+  if tweet
+    @tweet.update(params[:tweet])
+  end
+  redirect "/"
+end
+
+destroy "/tweets/:id" do
+  @tweet = Tweet.get([:id])
+  if @tweet
+    @tweet.destroy
+  end
+  redirect "/"
+end
